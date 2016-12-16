cdd8834d09288fecea161f795fdd59e6bacdede6
jiminchoi <Producer@ChoiJimin-no-MacBook-Pro.local>
2016-12-16 23:57:47 +0900

    Initial Commit

---
+++ HKR20161217Tests/HKR20161217Tests.swift
@@ -0,0 +1,36 @@
+//
+//  HKR20161217Tests.swift
+//  HKR20161217Tests
+//
+//  Created by ChoiJimin on 2016/12/16.
+//  Copyright © 2016年 ChoiJimin. All rights reserved.
+//
+
+import XCTest
+@testable import HKR20161217
+
+class HKR20161217Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
